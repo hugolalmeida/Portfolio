5308363aaf2f9dfbe8ae0c6fec29746bfb624594
hugolalmeida <hugogule@gmail.com>
2024-04-09 22:59:46 -0300

    Added some projects and some style and script

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -5,3 +5,18 @@ let year = (document.querySelector(".currentyear").textContent =
 
 let last_update = (document.querySelector("#jscript").textContent =
   document.lastModified);
+
+
+// Toggle language switch
+const languageSwitch = document.getElementById('languageSwitch');
+
+languageSwitch.addEventListener('change', () => {
+    // You can add logic here to change the language of your portfolio
+    if (languageSwitch.checked) {
+        // Change to language B
+        console.log('Switched to Language B');
+    } else {
+        // Change to language A
+        console.log('Switched to Language A');
+    }
+});
